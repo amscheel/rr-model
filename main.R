dad6bbdaa4daf5815f4023dba4c3a92ea2440ded
Anne Scheel <anne-scheel@gmx.de>
2021-10-01 08:07:41 +0200

    added internal loop to increase length of a generation; fixed bug in plot; improved plot

--- main.R
+++ main.R
@@ -9,14 +9,18 @@ library(ggplot2)
 payoff_RR <- 0.5  #payoff for a Registered Report (fixed)
 payoff_normal_pos <- 1  #payoff for a positive result published as a normal paper
 payoff_normal_neg <- 0  #payoff for a negative result published as a normal paper
+cost_RR <- 0 #cost of conducting a RR (subtracted from payoff)
+cost_normal <- 0 #cost of publishing a normal paper (subtracted from payoff)
 
 # Error rates for hypothesis tests
 # alpha_error <- 0  #alpha error, perhaps for later
 # beta_error <- 0  #beta error, perhaps for later
 
 # Population size & number of generations
-researcher_ID <-  seq(1:1e4) # initialise researcher population & set pop size
-generations <- 100 # set number of generations the model will loop through
+researcher_ID <-  seq(1:1e3) # initialise researcher population & set pop size
+generations <- 1e3 # set number of generations the model will loop through
+generation_duration <- 1
+mutation_sd <- .01
 
 #-----------------------------------------------------------------------#
 ## Model setup: create dataframe
@@ -36,15 +40,23 @@ df_loop$`0` <- runif(length(researcher_ID), 0,1) # initialise submission thresho
 #-----------------------------------------------------------------------#
 ## Here comes the loop:
 for (i in 1:generations) {
+  payoffs <- rep(0, length(researcher_ID))
   
+  for (j in 1:generation_duration) {
   ## 1. Research phase
   
   # 1.1 Each researcher is assigned a hypothesis with a random prior probability 
-  prior <- runif(length(researcher_ID), 0,1) 
+  #     I made two versions: the first is a uniform distribution between 0 and 1,
+  #     the second is a normal distribution with mean = .2, sd = .15, truncated
+  #     to 0-1. Comment/un-comment depending on preference
+  prior <- runif(length(researcher_ID), 0,1)
+  # prior <- pmin(pmax(rnorm(length(researcher_ID), mean = .25, sd = 0.15), 0),1)
+    # prior <- rep(1, length(researcher_ID))
   
   # 1.2 Researchers decide to run an RR or a normal study by comparing their
   #     current prior with their current submission threshold
   submit_as_RR <- ifelse(prior < df_loop[,1+i], 1, 0) 
+  # mean(result) - sum(prior)X
   
   # 1.3 Researcheres test their hypotheses and get either a positive or a 
   #     negative result (based on their prior)
@@ -52,9 +64,14 @@ for (i in 1:generations) {
   
   # 1.4 Researchers get a payoff depending on their chosen submission format
   #     and the result of their hypothesis test
-  payoffs <- ifelse(submit_as_RR == 1, payoff_RR,
-                            ifelse(result == 1, 
-                                   payoff_normal_pos, payoff_normal_neg))
+  # payoffs = result
+  # payoffs[submit_as_RR == 1] <- payoff_RR
+  # 
+  payoffs <- payoffs + ifelse(submit_as_RR == 1, payoff_RR-cost_RR,
+                            ifelse(result == 1,
+                                   payoff_normal_pos-cost_normal, payoff_normal_neg))
+
+  }
   
   # 1.5 Researchers' fitness is calculated based on their payoff,
   #     relative to the population
@@ -71,9 +88,10 @@ for (i in 1:generations) {
 
   # 2.2 New submission thresholds mutate
   #     (change sd to change the size of mutations)
-  evolved_submission_threshold <- round(evolved_submission_threshold + 
+  evolved_submission_threshold <- evolved_submission_threshold + 
                                           rnorm(length(researcher_ID), 
-                                                0, 0.02), 10) 
+                                                0, mutation_sd)
+  
   # Truncate the mutated numbers so that they're between 0 and 1
   evolved_submission_threshold <- pmin(pmax(evolved_submission_threshold, 0), 1)
   
@@ -84,90 +102,51 @@ for (i in 1:generations) {
 #-----------------------------------------------------------------------#
 # Turn the data frame into long format with numbers 0-100 for generation 
 # and one variable for evolving submission thresholds
-df_loop <- melt(df_loop,
+df_plot <- melt(df_loop,
                 id.vars = "researcher_ID",
                 variable.name = "generation",
                 value.name = "submission_threshold")
+df_plot$generation <- as.numeric(as.character(df_plot$generation))
 
-#-----------------------------------------------------------------------#
-# Plot the submission thresholds of every 10th generation:
-ggplot(
-  df_loop[as.numeric(as.character(df_loop$generation)) == seq(0,generations,10),],
-  # df_loop,
-  aes(x = as.numeric(as.character(generation)), y = submission_threshold)) +
-  geom_jitter(alpha = 0.5, width = 3, height = .001)+
-  geom_smooth(method = "lm", se = TRUE) +
-  scale_x_continuous(
-    breaks = seq(0,generations,10),
-    name = "generation") +
-  scale_y_continuous(lim=c(0,1), 
-                     breaks = seq(0,1,.1),
-                     name = "submission threshold")+
-  theme_bw(base_size = 14)
+eleven_generations <- seq(0, generations, generations/10)
 
+df_plot_subset <- df_plot[df_plot$generation == eleven_generations[1] |
+                       df_plot$generation == eleven_generations[2] |
+                       df_plot$generation == eleven_generations[3] |
+                       df_plot$generation == eleven_generations[4] |
+                       df_plot$generation == eleven_generations[5] |
+                       df_plot$generation == eleven_generations[6] |
+                       df_plot$generation == eleven_generations[7] |
+                       df_plot$generation == eleven_generations[8] |
+                       df_plot$generation == eleven_generations[9] |
+                       df_plot$generation == eleven_generations[10] |
+                       df_plot$generation == eleven_generations[11], ]
+##---------------------------------------------------------------------##
+## Plot the submission thresholds of every 10th generation
+# Basic plot setup:
+basic_plot <- ggplot(df_plot_subset,
+                     aes(x = generation,
+                       y = submission_threshold,
+                       # group = generation
+                     )) +
+  scale_x_continuous(breaks = seq(0, generations, generations / 10),
+                     name = "generation") +
+  scale_y_continuous(lim = c(0, 1),
+                     breaks = seq(0, 1, .1),
+                     name = "submission threshold") +
+  theme_bw() +
+  ggtitle(paste(generations, "generations", sep = " "),
+          subtitle = paste("population size: ", length(researcher_ID), "\n", 
+                           "generation length: ", generation_duration, 
+                           ifelse(generation_duration ==1, " round", " rounds"), "\n",
+                           "mutation size: ", mutation_sd, " SD",
+                           sep = ""))
 
-
-
-
-###===================================================================###
-### Below here is old code that we first wrote to set up the model    ###
-### without the loop (i.e. just two generations)                      ###
-###-------------------------------------------------------------------###
-
-researcher_ID <-  seq(1:1e4) #initialise first researcher population
-prior_hyp <- runif(length(researcher_ID), 0,1) #initialise priors for each researcher's hypothesis, uniformly distributed between 0 and 1
-submission_threshold <- runif(length(researcher_ID), 0, 1) #initialise 
-payoffs <- rep(NA, length(researcher_ID))
-# alpha <- rep(alpha_error, length(researcher_ID))
-# beta <-  rep(beta_error, length(researcher_ID))
-
-df <- data.frame(researcher_ID = researcher_ID,
-                 prior = prior_hyp,
-                 submission_threshold = submission_threshold, 
-                 payoffs = payoffs)
-
-#-----------------------------------------------------------------------#
-### potential extension for later, for now commented out: calculating
-### positive vs negative result based not only on the prior, but also
-### the alpha and beta error
-### IMPORTANT:  if used, df$result has to be changed to 
-###             rbinom(length(df$pos_result_prob), 1, df$pos_result_prob)
-##df$pos_result_prob <- (df$alpha*(1-df$prior)) + ((1-df$beta)*df$prior)
-
-#will_i_submit_as_normal_study <- df$prior < df$submission_threshold
-
-### SETTING SUBMISSION THRESHOLD: Connecting priors with publication format
-### CAUTION:  it matters which way round this is programmed (whether
-###           people submit RRs when their priors is BELOW or ABOVE
-###           the submission threshold)
-df$submit_as_RR <- ifelse(df$prior < df$submission_threshold, 1, 0)
-
-# #payoffs as expected value
-# df$payoffs <- ifelse(df$submit_as_RR == 1, payoff_RR,
-#                      df$prior * payoff_normal_pos + (1-df$prior) * payoff_normal_neg)
-
-#payoffs as either positive result OR negative result
-df$result <- rbinom(length(df$prior), 1, df$prior)  # gives researchers a positive result (1) with probability prior, otherwise 0
-df$payoffs <- ifelse(df$submit_as_RR == 1, payoff_RR,
-                     ifelse(df$result == 1, 
-                            payoff_normal_pos, payoff_normal_neg))
-
-# calculate fitness and manage reproduction
-df$fitness <- df$payoffs/sum(df$payoffs)
-
-#might wanna call this soemthiung else for clairty
-submission_threshold2 <- sample(df$submission_threshold, size = length(researcher_ID), replace = TRUE, prob = df$fitness)
-
-#change sd to change size of mutations
-submission_threshold2 <- round(submission_threshold2 + rnorm(length(researcher_ID), 0, 0.02)) 
-submission_threshold2 <- pmin(pmax(submission_threshold2, 0), 1)
-
-#let's see what selection favors
-hist(df$submission_threshold - submission_threshold2)
+# Show plot with jitter and regression line:
+basic_plot +
+  geom_jitter(alpha = 0.5, width = generations*.03, height = .001) +
+  geom_smooth(method = "lm", se = TRUE)
 
 
 
 
-  
-  
-  
